46925fb11c29b1cfde75deb60a1f5e609c3241ae
areiasz <133945329+areiasz@users.noreply.github.com>
2024-06-21 00:00:11 +0100

    enemies and counter

--- maingamee/src/scenes/FirstRealm/Scene2.js
+++ maingamee/src/scenes/FirstRealm/Scene2.js
@@ -2,6 +2,8 @@ class Scene2 extends Phaser.Scene {
     constructor() {
         super("Scene2");
         this.enemies = {};
+        this.enemyCount = 11;
+        this.enemyCountText = null;
     }
 
     preload() {
@@ -10,32 +12,97 @@ class Scene2 extends Phaser.Scene {
 
     create() {
         //this.add.text(400, 300, 'This is Scene2', { fontSize: '32px', fill: '#fff' }).setOrigin(0.5);
-        const background = this.add.image(800,800, 'back');
+
+        const background = this.add.image(800, 800, 'back');
         background.setScrollFactor(0);
-    
+
         //this.add.image(0,0,'slime')
         //this.add.image(400, 300, 'tile');
-        const map2 = this.make.tilemap({ key: 'map2'});
+        const map2 = this.make.tilemap({ key: 'map2' });
         const tileset1 = map2.addTilesetImage('tileset1', 'tile');
         const tileTrees = map2.addTilesetImage('trees', 'trees');
-    
+
+        const tilesize = 32;
+
+        enemies = this.physics.add.group();
+        this.enemies['slime1'] = new Slime(this, 16 * tilesize, 46 * tilesize);
+        enemies.add(this.enemies['slime1']);
+
+        this.enemies['slime2'] = new Slime(this, 36 * tilesize, 46 * tilesize);
+        enemies.add(this.enemies['slime2']);
+
+        this.enemies['slime3'] = new Slime(this, 59 * tilesize, 59 * tilesize);
+        enemies.add(this.enemies['slime3']);
+
+        this.enemies['slime4'] = new Slime(this, 67 * tilesize, 59 * tilesize);
+        enemies.add(this.enemies['slime4']);
+
+        this.enemies['slime5'] = new Slime(this, 5 * tilesize, 89 * tilesize);
+        enemies.add(this.enemies['slime5']);
+
+        this.enemies['slime6'] = new Slime(this, 77 * tilesize, 33 * tilesize);
+        enemies.add(this.enemies['slime6']);
+
+        this.enemies['slime7'] = new Slime(this, 46 * tilesize, 94 * tilesize);
+        enemies.add(this.enemies['slime7']);
+
+        this.enemies['slime8'] = new Slime(this, 98 * tilesize, 94 * tilesize);
+        enemies.add(this.enemies['slime8']);
+
+        this.enemies['slime9'] = new Slime(this, 66 * tilesize, 96 * tilesize);
+        enemies.add(this.enemies['slime9']);
+
+        this.enemies['slime10'] = new Slime(this, 63 * tilesize, 84 * tilesize);
+        enemies.add(this.enemies['slime10']);
+
+        this.enemies['slime11'] = new Slime(this, 88 * tilesize, 70 * tilesize);
+        enemies.add(this.enemies['slime11']);
+
+
         platforms = map2.createLayer('Ground', tileset1);
-        platforms.setCollisionByProperty({ collides: true});
-        
+        platforms.setCollisionByProperty({ collides: true });
+
         trees = map2.createLayer('Trees', tileTrees);
 
         // Create player
         player = new Player(this, 100, 450);
 
         this.physics.add.collider(player, platforms);
-    
-    
+        this.physics.add.collider(enemies, platforms);
+
+
         this.cameras.main.startFollow(player);
         this.cameras.main.setBounds(0, 0, map2.widthInPixels, map2.heightInPixels);
+        //this.cameras.main.setZoom(0.5);
         this.physics.world.setBounds(0, 0, map2.widthInPixels, map2.heightInPixels); // Define os limites do mundo conforme o tamanho do mapa
-    
+
+        this.enemyCountText = this.add.text(1200, 32, `Inimigos restantes: ${Object.keys(this.enemies).length}`, {
+            fontFamily: 'Consolas',
+            fontSize: '30px',
+            fill: '#05ceec',
+            padding: {
+                x: 8,
+                y: 4
+            },
+            shadow: {
+                offsetX: 2,
+                offsetY: 2,
+                color: '#000000',
+                blur: 3,
+                fill: true
+            }
+        });
+
+        this.enemyCountText.setScrollFactor(0);
+
         //this.scene.start("Scene2");
-        
+
+    }
+
+    createEnemy(key, x, y) {
+        this.enemies[key] = new Slime(this, x, y);
+        this.enemiesGroup.add(this.enemies[key]);
+        this.enemyCount++; // Incrementa o contador de inimigos
     }
 
     update() {
@@ -52,8 +119,14 @@ class Scene2 extends Phaser.Scene {
         for (const enemyKey in this.enemies) {
             const enemy = this.enemies[enemyKey];
             if (!enemy.active) {
-               delete this.enemies[enemyKey];
+                delete this.enemies[enemyKey];
+                this.enemyCount--; // Decrementa o contador de inimigos
+                this.updateEnemyCountText(); // Atualiza o texto da contagem de inimigos
             }
         }
+    }
+
+    updateEnemyCountText() {
+        this.enemyCountText.setText(`Inimigos restantes: ${this.enemyCount}`);
     }
 }
--- maingamee/src/scenes/FirstRealm/Scene3.js
+++ maingamee/src/scenes/FirstRealm/Scene3.js
@@ -2,6 +2,8 @@ class Scene3 extends Phaser.Scene {
     constructor() {
         super("Scene3");
         this.enemies = {};
+        this.enemyCount = 17;
+        this.enemyCountText = null;
     }
 
     preload() {
@@ -12,20 +14,74 @@ class Scene3 extends Phaser.Scene {
 
         const mapWidth = 3200;
         const mapHeight = 3200;
-        
-        const background = this.add.image(1600,1600, 'back2');
+
+        const background = this.add.image(1600, 1600, 'back2');
         //background.setScrollFactor(0);
         background.setScale(mapWidth / background.width, mapHeight / background.height);
-    
+
         //this.add.image(0,0,'slime')
         //this.add.image(400, 300, 'tile');
-        const map3 = this.make.tilemap({ key: 'map3'});
+        const map3 = this.make.tilemap({ key: 'map3' });
         const tileset1 = map3.addTilesetImage('tileset1', 'tile');
         const tileTrees = map3.addTilesetImage('trees', 'trees');
-    
+
+        const tilesize = 32;
+
+        enemies = this.physics.add.group();
+        this.enemies['slime1'] = new Slime(this, 26 * tilesize, 10 * tilesize);
+        enemies.add(this.enemies['slime1']);
+
+        this.enemies['slime2'] = new Slime(this, 95 * tilesize, 15 * tilesize);
+        enemies.add(this.enemies['slime2']);
+
+        this.enemies['slime3'] = new Slime(this, 70 * tilesize, 38 * tilesize);
+        enemies.add(this.enemies['slime3']);
+
+        this.enemies['slime4'] = new Slime(this, 45 * tilesize, 33 * tilesize);
+        enemies.add(this.enemies['slime4']);
+
+        this.enemies['slime5'] = new Slime(this, 24 * tilesize, 47 * tilesize);
+        enemies.add(this.enemies['slime5']);
+
+        this.enemies['slime6'] = new Slime(this, 8 * tilesize, 95 * tilesize);
+        enemies.add(this.enemies['slime6']);
+
+        this.enemies['slime7'] = new Slime(this, 35 * tilesize, 98 * tilesize);
+        enemies.add(this.enemies['slime7']);
+
+        this.enemies['slime8'] = new Slime(this, 58 * tilesize, 96 * tilesize);
+        enemies.add(this.enemies['slime8']);
+
+        this.enemies['slime9'] = new Slime(this, 58 * tilesize, 70 * tilesize);
+        enemies.add(this.enemies['slime9']);
+
+        this.enemies['slime10'] = new Slime(this, 38 * tilesize, 65 * tilesize);
+        enemies.add(this.enemies['slime10']);
+
+        this.enemies['slime11'] = new Slime(this, 82 * tilesize, 61 * tilesize);
+        enemies.add(this.enemies['slime11']);
+
+        this.enemies['slime12'] = new Slime(this, 98 * tilesize, 63 * tilesize);
+        enemies.add(this.enemies['slime12']);
+
+        this.enemies['slime13'] = new Slime(this, 3 * tilesize, 81 * tilesize);
+        enemies.add(this.enemies['slime13']);
+
+        this.enemies['slime14'] = new Slime(this, 98 * tilesize, 63 * tilesize);
+        enemies.add(this.enemies['slime14']);
+
+        this.enemies['slime15'] = new Slime(this, 59 * tilesize, 13 * tilesize);
+        enemies.add(this.enemies['slime15']);
+
+        this.enemies['slime16'] = new Slime(this, 95 * tilesize, 80 * tilesize);
+        enemies.add(this.enemies['slime16']);
+
+        this.enemies['slime17'] = new Slime(this, 80 * tilesize, 14 * tilesize);
+        enemies.add(this.enemies['slime17']);
+
         platforms = map3.createLayer('Ground', tileset1);
-        platforms.setCollisionByProperty({ collides: true});
-        
+        platforms.setCollisionByProperty({ collides: true });
+
         trees = map3.createLayer('Trees', tileTrees);
 
         //back = map3.createLayer('Background', tileset1);
@@ -34,14 +90,41 @@ class Scene3 extends Phaser.Scene {
         player = new Player(this, 100, 100);
 
         this.physics.add.collider(player, platforms);
-    
-    
+        this.physics.add.collider(enemies, platforms);
+
+
         this.cameras.main.startFollow(player);
+        //this.cameras.main.setZoom(0.5);
         this.cameras.main.setBounds(0, 0, map3.widthInPixels, map3.heightInPixels);
         this.physics.world.setBounds(0, 0, map3.widthInPixels, map3.heightInPixels); // Define os limites do mundo conforme o tamanho do mapa
-    
+
+        //Texto com a informação dos inimigos restantes
+        this.enemyCountText = this.add.text(1200, 32, `Inimigos restantes: ${Object.keys(this.enemies).length}`, {
+            fontFamily: 'Consolas',
+            fontSize: '30px',
+            fill: '#05ceec',
+            padding: {
+                x: 8,
+                y: 4
+            },
+            shadow: {
+                offsetX: 2,
+                offsetY: 2,
+                color: '#00C954',
+                blur: 2,
+                fill: true
+            }
+        });
+        this.enemyCountText.setScrollFactor(0);
+
         //this.scene.start("Scene2");
-        
+
+    }
+
+    createEnemy(key, x, y) {
+        this.enemies[key] = new Slime(this, x, y);
+        this.enemiesGroup.add(this.enemies[key]);
+        this.enemyCount++; // Incrementa o contador de inimigos
     }
 
     update() {
@@ -54,8 +137,14 @@ class Scene3 extends Phaser.Scene {
         for (const enemyKey in this.enemies) {
             const enemy = this.enemies[enemyKey];
             if (!enemy.active) {
-               delete this.enemies[enemyKey];
+                delete this.enemies[enemyKey];
+                this.enemyCount--; // Decrementa o contador de inimigos
+                this.updateEnemyCountText(); // Atualiza o texto da contagem de inimigos
             }
         }
+    }
+
+    updateEnemyCountText() {
+        this.enemyCountText.setText(`Inimigos restantes: ${this.enemyCount}`);
     }
 }
